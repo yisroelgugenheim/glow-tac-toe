c68493fb5e2a5465f7d44539d4bcec5873c577a2
Your Name <you@example.com>
2023-10-12 16:24:16 -0700

    minor touch-ups

--- p4.js
+++ p4.js
@@ -1,42 +1,40 @@
 const cells = document.querySelectorAll('.cell')
 const stat = document.getElementById('win-status')
-let currentPlayer = 'X';
+let currentPlayer = 'X'
 
-
+// handle clicks
 for(let cell of cells){
     cell.addEventListener('click',()=> {
     if(cell.innerText === ''){
     cell.innerText = currentPlayer;
     if (currentPlayer === 'X') { currentPlayer = 'O'}
     else { currentPlayer = 'X'}}
+// handle wins
     if(XWins()) {
-    stat.innerText = 'X wins! Good game! Restart to play again.'
-    for(let cell of cells) {
-    cell.addEventListener('click', ()=> {
-        cell.innerText = ''
-    })
- }
+      stat.innerText = 'X wins! Good game! Restart to play again.'
+      currentPlayer = ''
     } else if(OWins()) {
         stat.innerText = 'O wins!! Good game! Restart to play again.'
-        for(let cell of cells) {
-        cell.addEventListener('click', ()=> {
-            cell.innerText = ''
-        })
-     }
-    }
+        currentPlayer = ''
+      }
    })
 }
 
+
+
+
 function restartGame() {
 let button = document.querySelector('button')
-button.addEventListener('click', ()=> {
+check = button.addEventListener('click', ()=> {
+
 for(let cell of cells) {
     cell.innerText = ''
     stat.innerText = ''
-}
+}   XWins() = false
+    OWins() = false
 })
-}
-restartGame()
+} restartGame()
+
 
 
 
@@ -55,10 +53,10 @@ restartGame()
 
 // for(let cell of cells){board.push(cell)}
 // let board = [];
-const wins = [
-    [0,1,2], [3,4,5], [6,7,8],
-    [0,3,6], [1,4,7], [2,5,8],
-    [0,4,8], [2,4,6]];
+// const wins = [
+//     [0,1,2], [3,4,5], [6,7,8],
+//     [0,3,6], [1,4,7], [2,5,8],
+//     [0,4,8], [2,4,6]];
 
 
 
